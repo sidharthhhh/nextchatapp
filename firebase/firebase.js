92c75e8844a5b970a5c055eb191395b9bdc71e20
Jai Shukla <iamshuklajai@gmail.com>
2023-06-23 05:55:22 +0530

    created search option and side navbar
    i have completly setup firebase and storages
    
    i have also created the sidebar for profile and signout
    
    signin and register functions are working fine

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,9 +2,7 @@ import { initializeApp } from "firebase/app";
 
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import { getFireStore } from "firebase/firestore";
-
-
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCcXuflaQd4GfoqyTiBqQp99Tazbfnq2Q0",
@@ -12,13 +10,10 @@ const firebaseConfig = {
   projectId: "next-chat-app-fef8e",
   storageBucket: "next-chat-app-fef8e.appspot.com",
   messagingSenderId: "350202439642",
-  appId: "1:350202439642:web:acd6c9eaa114f6cc29c2e3"
+  appId: "1:350202439642:web:acd6c9eaa114f6cc29c2e3",
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
-export const db = getFireStore(app);
-
-
-
+export const db = getFirestore(app);
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 
+import { useAuth } from "@/context/authContext";
+import { useRouter } from "next/router";
+import Loader from "@/components/Loader";
+import LeftNav from "@/components/LeftNav";
+import Chats from "@/components/Chats";
 const Home = () => {
-  return <div>Home</div>;
+  const router = useRouter();
+  const { signOut, currentUser, isLoading } = useAuth();
+
+  useEffect(() => {
+    if (!isLoading && !currentUser) {
+      router.push("/login");
+    }
+  }, [currentUser, isLoading]);
+
+  return !currentUser ? (
+    <Loader />
+  ) : (
+    // <div>
+    //     <button onClick={signOut}>Sign Out</button>
+    // </div>
+    <div className="bg-c1 flex h-[100vh]">
+      <div className="flex w-full shrink-0">
+        <LeftNav />
+
+        <div className="flex bg-c2 grow">
+          <div className="w-[400px] p-5 overflow-auto scrollbar shrink-0 border-r border-white/[0.05]">
+            <div className="flex flex-col h-full">
+              <Chats />
+            </div>
+          </div>
+          <div>Chat</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
