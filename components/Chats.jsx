986136d5d5a7cd0bc2f2dc5452967c7e1191de11
Sidharth Shukla <swayam1044@gmail.com>
2023-06-23 08:49:12 +0530

    24:49

--- components/Chats.jsx
+++ components/Chats.jsx
@@ -3,10 +3,21 @@ import { useChatContext } from "@/context/chatContext";
 import { collection, onSnapshot } from "firebase/firestore";
 import { db } from "@/firebase/firebase";
 import { RiSearch2Line } from "react-icons/ri";
+import Avatar from "./Avatar";
+import { useAuth } from "@/context/authContext";
+
+
 
 const Chats = () => {
-  const { users, setUsers } = useChatContext();
+  const {
+     users,
+     setUsers,
+     selectedChat,
+     serSelectedChat,
+     chats, 
+     setChats, } = useChatContext();
   const [search, setSearch] = useState("");
+  const {currentUser} = useAuth()
 
   useEffect(() => {
     onSnapshot(collection(db, "users"), (snapshot) => {
@@ -19,6 +30,23 @@ const Chats = () => {
     });
   }, []);
 
+  useEffect(()=>{
+    const getChats = () =>{
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc)=>{
+        if(doc.exits()){
+          const data = doc.data();
+          setChats(data);
+        }
+      })
+    }
+    currentUser.uid && getChats();
+  }, []);
+
+  const filteredChats = Object.entries(chats || {})
+  .sort((a,b)=>b[1].date-a[1].date)
+
+  // console.log((filteredChats))
+
   return (
     <div className="flex flex-col h-full">
       <div className="shrink-0 sticky -top-[20px] z-10 flex justify-center w-full bg-c2 py-5">
@@ -30,6 +58,22 @@ const Chats = () => {
           placeholder="Search username..."
           className="w-[300px] h-12 rounded-xl bg-c1/[0.5] pl-11 pr-5 placeholder:text-c3 outline-none text-base"
         />
+
+
+
+          {/* yaha ul li tha...wo kaha gaya gaya */}
+
+
+        <Avatar size= "x-larg" user={currentUser} />
+        <div className="flex flex-col gap-1 grow relative">
+          <span className="text-base text-white flex items-center justify-between">
+            <div className="font-medium ">displayName</div>
+            <div className="text-c3 text-xs">date</div>
+          </span>
+          <p className="text-sm text-c3 line-clamp-1 break-all">Last message</p>
+
+          <span className="absolute right-0 top-7 min-w-[20px] h-5 rounded-full bg-red-500 justify-center items-center text-sm">5</span>
+        </div>
       </div>
     </div>
   );
