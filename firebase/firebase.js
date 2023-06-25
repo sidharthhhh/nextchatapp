1363a4f47e11cbaec4e110720042fb8782fc7b11
Sidharth Shukla <swayam1044@gmail.com>
2023-06-25 14:51:59 +0530

    final

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -9,8 +9,8 @@ const firebaseConfig = {
   authDomain: "next-chat-app-fef8e.firebaseapp.com",
   projectId: "next-chat-app-fef8e",
   storageBucket: "next-chat-app-fef8e.appspot.com",
-  messagingSenderId: "350202439642",
-  appId: "1:350202439642:web:acd6c9eaa114f6cc29c2e3",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
