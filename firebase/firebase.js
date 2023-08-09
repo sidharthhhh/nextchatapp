ce6bfd866d99d1604eb05bfa5e5c09968c9d07f5
Jai Shukla <iamshuklajai@gmail.com>
2023-08-10 00:58:12 +0530

    issue fixed

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -5,15 +5,15 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyCZE-7pgeMc6g5sahQhZY9sWx5VhjF1fwM",
+  authDomain: "chatapp-e9f45.firebaseapp.com",
+  projectId: "chatapp-e9f45",
+  storageBucket: "chatapp-e9f45.appspot.com",
+  messagingSenderId: "14404146429",
+  appId: "1:14404146429:web:b622625f8068d357f5da94"
 };
-
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const storage = getStorage(app);
 export const db = getFirestore(app);
